c724e1c4ad5eaa021c15e2a0aa553bb1c3850fba
Tim Traylor <ttraylor83@gmail.com>
2022-11-02 15:39:05 +0000

    some style work and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ToDo from './components/ToDo/ToDo';
 import Header from './components/Header/Header';
+import SettingsPage from './pages/Settings';
+// import SettingsLink from './pages/SettingLink';
+import './styles.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <ToDo />
-    </div>
+    <Router>
+      <div className='app-container'>
+        <Header />
+
+        <Routes>
+
+          <Route exact path='/' element={
+            <>
+              <ToDo />
+            </>
+          }>
+          </Route>
+
+          <Route path='/settings' element={<SettingsPage />} />
+
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+function SettingsPage() {
+  return (
+    <>
+      <div className='configureSettings'>
+        Alter the basic things
+
+      </div>
+    </>
+  )
+}
+export default SettingsPage;
